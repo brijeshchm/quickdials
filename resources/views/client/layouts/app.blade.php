862b78bc3a522ca8cebb5bbd84467bc0b43b10a0
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-02-27 14:31:00 +0530

    canonical

--- resources/views/client/layouts/app.blade.php
+++ resources/views/client/layouts/app.blade.php
@@ -8,8 +8,12 @@
     <meta name="keywords" content="@yield('keyword')">
     <meta name="description" content="@yield('description')">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="canonical" href="{{ URL::current() }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">  
+    @if (Request::is('/'))
+    <link rel="canonical" href="{{ url('/') }}/" />
+    @else
+        <link rel="canonical" href="{{ url()->current() }}" />
+    @endif    
     <link rel="shortcut icon" href="{{asset('client/images/favicon.png')}}" type="image/png" />
     <meta http-equiv="content-language" content="en-IN">
     <meta name="classification" content="directory portal" />
