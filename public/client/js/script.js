423e079b3545f75afab8510d88f9967e7dec700e
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-03-06 20:39:00 +0530

    script

--- public/client/js/script.js
+++ public/client/js/script.js
@@ -865,7 +865,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		$(document).on('click', '.keystore', function (e) {
 		 
 			e.preventDefault();
-			var slug = $(this).attr('href');				 
+			var slug = $(this).attr('href');	
+			console.log(window.location.href);			 
 			slug = slug.replace(window.location.href, '');	 	 
 			slug = slug.replace(/\s+/g, '-').toLowerCase();				
 			text = slug.replace(/-/g, ' ');
