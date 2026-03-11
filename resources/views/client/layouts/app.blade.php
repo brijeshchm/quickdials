7a57fa85b5a3844219ab3f8c2d5dd3fd59e54e9f
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-03-12 00:24:04 +0530

    tag m

--- resources/views/client/layouts/app.blade.php
+++ resources/views/client/layouts/app.blade.php
@@ -58,7 +58,19 @@
         gtag('js', new Date());
         gtag('config', 'G-0B7X99VQ0W');
     </script> -->
+
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KF6W10RN9L"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KF6W10RN9L');
+</script>
     <!-- Google Tag Manager -->
+
+
           
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
